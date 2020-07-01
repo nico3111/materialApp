1d444260e388cd0d248c56ca4f78192a4ac5ab5f
nico3111 <nico.purin@hotmail.com>
2020-07-01 09:03:13 +0200

    m

--- src/components/notebook/ShowAllNotebooks.jsx
+++ src/components/notebook/ShowAllNotebooks.jsx
@@ -13,7 +13,7 @@ export default class ShowAllNotebook extends React.Component {
         const url = "https://localhost:44323/Material/Notebook";
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({ person: data.results[0], loading: false });
+        this.setState({ person: data.results, loading: false });
 
     }
 
