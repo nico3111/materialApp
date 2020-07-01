2c785232780d213e3edbb7469c17f91393316978
MartinHir <m.hirschbuehl@hotmail.com>
2020-07-01 10:41:13 +0200

    implemented Update in API

--- MaterialData/repository/NotebookRepository.cs
+++ MaterialData/repository/NotebookRepository.cs
@@ -35,5 +35,21 @@ namespace MaterialData
             entities.notebook.FromSqlRaw("ALTER TABLE notebook AUTO_INCREMENT = 1;");
             entities.SaveChanges();
         }
+
+        public void Update(notebook notebook)
+        {
+            var existingNotebook = entities.notebook.FirstOrDefault(x => x.id == notebook.id);
+
+            if (existingNotebook != null)
+            {
+                existingNotebook.serial_number = notebook.serial_number;
+                existingNotebook.make = notebook.make;
+                existingNotebook.model = notebook.model;
+                existingNotebook.person_id = notebook.person_id;
+                existingNotebook.location_id = notebook.location_id;
+
+                entities.SaveChanges();
+            }
+        }
     }
 }
--- MaterialREST/Startup.cs
+++ MaterialREST/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace MaterialREST
 {
@@ -35,7 +28,7 @@ namespace MaterialREST
                                 .AllowAnyHeader();
                     });
 
-                
+
             });
             services.AddControllers();
         }
@@ -47,7 +40,7 @@ namespace MaterialREST
             {
                 app.UseDeveloperExceptionPage();
             }
-            
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -61,7 +54,7 @@ namespace MaterialREST
                 endpoints.MapControllers();
             });
 
-            
+
         }
     }
 }
