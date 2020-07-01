dbe999151134716364ceff3bbbe7ed846a3e3753
MartinHir <m.hirschbuehl@hotmail.com>
2020-07-01 10:02:48 +0200

    added DELETE in REST

--- MaterialData/repository/NotebookRepository.cs
+++ MaterialData/repository/NotebookRepository.cs
@@ -1,5 +1,6 @@
 ﻿using MaterialData.models;
 using MaterialData.repository;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -31,6 +32,7 @@ namespace MaterialData
         public void Delete(notebook notebook)
         {
             entities.notebook.Remove(notebook);
+            entities.notebook.FromSqlRaw("ALTER TABLE notebook AUTO_INCREMENT = 1;");
             entities.SaveChanges();
         }
     }
