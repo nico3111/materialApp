849d2b9452cdbe566ea38eee680babe1d0ff5a3a
nico3111 <nico.purin@hotmail.com>
2020-07-13 15:22:35 +0200

    changed url

--- src/components/books/BookModal.jsx
+++ src/components/books/BookModal.jsx
@@ -132,7 +132,7 @@ export default class BookModal extends React.Component {
                 body: JSON.stringify(body)
             }
 
-            let result = await fetch('http://192.168.0.94:8015/material/notebook/', req)
+            let result = await fetch('http://192.168.0.94:8015/material/book/', req)
             const r = await result.text()
             if (r !== "")
                 alert(r)
